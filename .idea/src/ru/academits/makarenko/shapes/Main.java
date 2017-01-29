95f80ff18f742c4c6e3d9151f08e1511b44ac5bd
1 <i_616@mail.ru>
2017-01-29 12:37:37 +0700

    Первый коммит

---
+++ .idea/src/ru/academits/makarenko/shapes/Main.java
@@ -0,0 +1,10 @@
+package ru.academits.makarenko.shapes;
+
+/**
+ * Created by 1 on 29.01.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
